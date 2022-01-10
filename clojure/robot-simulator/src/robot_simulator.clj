db45e62572b3a0ca3676eb5451e42951eec5684e
Logan Waite <lwaite@oceanx.com>
2022-01-10 14:45:51 -0500

    clojure: start collatz conjecture

--- clojure/robot-simulator/src/robot_simulator.clj
+++ clojure/robot-simulator/src/robot_simulator.clj
@@ -1,25 +1,27 @@
-(ns robot-simulator
-  (:require [clojure.string :as str]))
+(ns robot-simulator)
 
-(defn robot [coordinates bearing] 
+(defn robot 
+  [coordinates bearing] 
   {:coordinates coordinates :bearing bearing})
 
 (def turn-right {:north :east :east :south :south :west :west :north})
 
 (def turn-left {:north :west :east :north :south :east :west :south})
 
-(defn advance [bearing {:keys [x y]}]
+(defn advance 
+  [{:keys [x y]} bearing]
   (case bearing
     :north {:x x :y (inc y)}
     :south {:x x :y (dec y)}
     :east {:x (inc x) :y y}
     :west {:x (dec x) :y y}))
 
-(defn do-action [{:keys [bearing coordinates]} action]
+(defn do-action 
+  [{:keys [bearing] :as robot} action]
   (case action
-    \R {:bearing (turn-right bearing) :coordinates coordinates}
-    \L {:bearing (turn-left bearing) :coordinates coordinates}
-    \A {:bearing bearing :coordinates (advance bearing coordinates)}))
+    \R (update robot :bearing turn-right)
+    \L (update robot :bearing turn-left)
+    \A (update robot :coordinates advance bearing)))
 
 (defn simulate [path robot]
   (reduce do-action robot path))
